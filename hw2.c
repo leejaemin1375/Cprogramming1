67d64d252a9edcb60ba237596b595eabf40ce3f2
leejaemin1375 <126881963+leejaemin1375@users.noreply.github.com>
2023-09-22 18:13:03 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+int main(void)
+{
+    float num1;
+    double result;
+
+    printf("Please enter kilometers:");
+    scanf_s("%f", &num1);
+    result = num1 / 1.609;
+    printf("%.1f km is equal to %.1f miles", num1, result);
+    return 0;
+}
