438af56719728f3cbc399f15d4a50fa2a9b2e1c8
leejaemin1375 <126881963+leejaemin1375@users.noreply.github.com>
2023-11-24 11:11:08 +0900

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+double calculateStandardDeviation(double arr[], int n);
+
+int main() {
+	int n = 5;
+	double numbers[5];
+
+
+	printf("Enter 5 real numbers:");
+	for (int i = 0; i < n; ++i) {
+		scanf_s("%lf", &numbers[i]);
+	}
+
+	double stdDev = calculateStandardDeviation(numbers, n);
+	printf("\nStandard Deviation = %.3lf", stdDev);
+
+	return 0;
+}
+
+double calculateStandardDeviation(double arr[], int n) {
+	double mean = 0.0, variance = 0.0, stdDev = 0.0;
+
+
+	for (int i = 0; i < n; ++i) {
+		mean += arr[i];
+	}
+	mean /= n;
+
+
+	for (int i = 0; i < n; ++i) {
+		variance += pow(arr[i] - mean, 2);
+	}
+	variance /= n;
+
+	stdDev = sqrt(variance);
+
+	return stdDev;
+}
